4a8f8a069816ea656a5eca733cf736e3ab5685cc
alutterbein <alutterbein@gmail.com>
2024-06-22 17:56:53 -0700

    updated index.js with questions to fullfil shape and color criteria

--- index.js
+++ index.js
@@ -11,17 +11,32 @@ const questions = [
 
 
 
+},
+{
+    name: "color",
+    type: "input",
+    message: "enter a color keyword or hexdecimal color value",
+
+
+
+},
+{
+    name: 'shape',
+    message: 'describe your readme',
+    type: 'list',
+    choices: ["circle", "square", "triangle"],
+
+
 }
-
-
 ]
 
 const generateLogo = () => {
 inquirer
     .prompt (questions)
-    .then(({ text }) => {
+    .then(({ text, color, shape }) => {
         const svg = new SVG();
         svg.setText(text);
+        svg.setShape(shape, color);
         return writeFile("logo.svg", svg.render());
     })
 .catch((error) => {
